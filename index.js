7e606ebb5d39bd1cdb54c9a9a9125937099ff3d3
Tomás <tomas_dani18@outlook.com>
2023-06-06 19:02:15 -0300

    m

--- index.js
+++ index.js
@@ -3,13 +3,29 @@ const express = require("express");
 // crear servidor
 const app = express();
 
+app.use(express.json()); // para poder leer json en el body
+
+require("./base-orm/sqlite-init");  // crear base si no existe
+
+app.use(express.json()); // para poder leer json en el body
+
 // controlar ruta
 app.get("/", (req, res) => {
   res.send("Backend inicial dds-backend!");
 });
 
+const articulosfamiliasmockRouter = require("./routes/articulosfamiliasmock");
+app.use(articulosfamiliasmockRouter);
+
+const articulosfamiliasRouter = require("./routes/articulosfamilias");
+app.use(articulosfamiliasRouter);
+
 // levantar servidor
-const port = 3000;
-app.listen(port, () => {
+if (!module.parent) {   // si no es llamado por otro modulo, es decir, si es el modulo principal -> levantamos el servidor
+  const port = process.env.PORT || 3000;   // en produccion se usa el puerto de la variable de entorno PORT
+  app.locals.fechaInicio = new Date();
+  app.listen(port, () => {
     console.log(`sitio escuchando en el puerto ${port}`);
-});
+  });
+}
+module.exports = app; // para testing
